342af3c94b75c579a2ac695a4f0bc46b83a631b4
bvgaurav4 <bv.gaurav4@gmail.com>
2024-09-15 23:46:18 +0530

    making the database in sql and testing some stuff

---
+++ my_sql.sql
@@ -0,0 +1,21 @@
+show databases;
+create database spread;
+use spread;
+create table spreadsheet(
+	spreadsheet_id varchar(100) not null PRIMARY KEY,
+    spreadsheet_name varchar(100),
+    owner varchar(100),
+    creation_date DATE not null
+);
+
+create table spreadsheet_data(
+	spreadsheet_id varchar(100) not null,
+    data_date DATE not null,
+    data_index varchar(100) not null,
+    x varchar(100) not null,
+    y varchar(100) not null,
+    data_value TEXT,
+    PRIMARY KEY (spreadsheet_id, data_index,x,y),
+    FOREIGN KEY (spreadsheet_id) REFERENCES spreadsheet(spreadsheet_id)
+);
+    
